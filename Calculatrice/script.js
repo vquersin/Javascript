3f19c0204547c6b10dbd72734dba8982583f57ae
vquersin <quersin.valentin@gmail.com>
2023-07-05 13:22:46 +0200

    calculatrice terminee

--- Calculatrice/script.js
+++ Calculatrice/script.js
@@ -1,47 +1,114 @@
 calc_memory = "";
 var calcul = 0;
+var result = 0;
+var entry = "";
 var dot = 0;
+var f = 0;
+var lastOperator = "";
 
-
-function calc(id,n){
+function calc(n){
     switch(n){
         case '/':
-            calc_memory += n;
-            break;
         case 'x':
-            calc_memory += n;
-            break;
         case '-':
-            calc_memory += n;
-            break;
         case '+':
-            calc_memory += n;
-            break;
+            if(f === 0){firstEntry(n);break;}
+            else{Operator(n);break;}
         case ',':
         case '.':
-            dot =1;
+            dot =1; // Variable permettant d'indiqué qu'on passe en décimale et servira pour l'exponentielle ...
             calc_memory += n;
+            entry += n;
             break;
         case 'CE':
-            calc_memory += "<br>";
-            calcul = 0;
+            Reset();
+            break;
+        case '=':
+            Equal(n);
             break;
         default:
+            entry += n;
             if(dot > 0){   
-                calcul += (n ** (dot*-1)); 
+                calcul += n * Math.pow(10, -dot); // Exponentielle pour les décimales ...
                 dot++; 
             }
             else{
-                if(calcul == 0){
-                    calcul = n;
-                }
-                else{
-                    calcul = (calcul*10)+n;
-                }
+                if(calcul == 0){calcul = n;}
+                else{calcul = (calcul*10)+n;}
             }
             calc_memory += n;
-            document.getElementById('memoryP').innerHTML = calc_memory;
+            break;
     }
-    console.log(calc_memory);
-    console.log(calcul);
+
+    // AFFICHAGE DES CADRANS ...
+    document.getElementById('memoryP').innerHTML = calc_memory;
+    document.getElementById('cadranResult').innerHTML = result;
+    document.getElementById('cadran').innerHTML = calcul;
+}
+
+// FUNCTION permettant l'assignation de valeurs de départ au début du calcul ...
+    // Pour éviter un maximun de bugs ...
+function firstEntry(n){
+    dot = 0;
+    result = calcul;
+    calcul = 0;
+    calc_memory += n;
+    lastOperator = n;
+    f++;
+}
+
+// FUNCTION permettant de calculer le résultat et faire des assignations ...
+    // Lors d'opérations ...
+function Operator(n){
+    console.log(lastOperator);
+    switch(lastOperator){
+        case '+':
+            result += calcul;
+            lastOperator = n;
+            dot = 0;
+            calcul = 0;
+            calc_memory += n;
+            break;
+        case '-':
+            result -= calcul;
+            lastOperator = n;
+            dot = 0;
+            calcul = 0;
+            calc_memory += n;
+            break;
+        case 'x':
+            result *= calcul;
+            lastOperator = n;
+            dot = 0;
+            calcul = 0;
+            calc_memory += n;
+            break;
+        case '/':
+            result /= calcul;
+            lastOperator = n;
+            dot = 0;
+            calcul = 0;
+            calc_memory += n;
+            break;
+    }
+}
+
+// RESET global avec passage à la ligne du panel Memory ...
+function Reset(){
+    calc_memory += "<br>";
+    entry = "";
+    calcul = 0;
+    result = 0;
+    dot = 0;
+    f = 0;
+}
+
+// FUNCTION permettant le calcul final et mettre fin à l'opération en cours ...
+function Equal(n){
+    Operator(n);
+    calc_memory += " " + result + "<br>";
+    entry = "";
+    calcul = 0;
+    dot = 0;
+    f = 0;
 }
